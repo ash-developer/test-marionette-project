d746910eab3132f8a4e13fd693702c026a139085
Andrei Shevel <ash.developer@yandex.ru>
2015-05-07 11:49:07 +0300

    display static view: exercise

--- src/app/app.js
+++ src/app/app.js
@@ -4,18 +4,31 @@
 
     var application = new Marionette.Application();
 
-    application.TestView = Marionette.ItemView.extend({
-        id: 'test-view',
-        tagName: 'span',
-        className: 'test-view',
-        template: "#test-template"
+    application.ListView = Marionette.ItemView.extend({
+        tagName: 'ul',
+        className: 'list',
+        template: function () {
+            var html = '';
+
+            for (var i = 0; i < 10; i++) {
+                var item = new application.ItemView();
+                html += item.render().$el.html();
+            }
+
+            return html;
+        }
+    });
+
+    application.ItemView = Marionette.ItemView.extend({
+        className: 'list-item',
+        template: '#list-item-template'
     });
 
     application.on("start", function () {
         console.log('Marionette application was started!');
 
-        var testView = new application.TestView();
-        $('#content').html(testView.render().$el);
+        var list = new application.ListView();
+        $('#content').html(list.render().$el);
     });
 
     application.start();
